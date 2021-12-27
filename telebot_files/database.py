da3fdfe096f1d4d816d0bf31f254b5a67243934d
Bryanleongys <bryanleongys@gmail.com>
2021-12-27 23:20:49 +0800

    edited db queries for User and EventsCustomChoices table

--- telebot_files/database.py
+++ telebot_files/database.py
@@ -9,7 +9,10 @@ CONSTANTS
 '''
 
 EVENT_MESSAGE = 6
-USER_NAME = 0
+USER_ID = 0
+WIN_COUNT = 6
+EVENT_ID = 0
+USER_NAME = 1
 START_DATE = 1
 END_DATE = 2
 COLLECTION_DATE = 3
@@ -42,13 +45,13 @@ class Database:
                 '''CREATE TABLE Events(EventID integer primary key, EventName text, StartDate text, EndDate text, CollectionDate text, 
                     StartTime text, EndTime text, Message text)''')
             self.cur.execute(
-                '''CREATE TABLE Users(UserID integer primary key, UserName text, NusnetId text, House text, TelegramId text. TelegramHandle text, WinCount integer)''')
+                '''CREATE TABLE Users(UserID integer primary key, UserName text, NusnetId text, House text, TelegramId text, TelegramHandle text, WinCount integer)''')
             self.cur.execute(
                 '''CREATE TABLE EventsJoined(UserID integer not null, EventID integer not null, Timing text, ItemChosen text,
                 FOREIGN KEY(UserID) REFERENCES Users(UserID),
                 FOREIGN KEY(EventID) REFERENCES Events(EventID))''')
             self.cur.execute(
-                '''CREATE TABLE EventsCustomChoices(EventID integer not null, ChoiceHeader text, ChoiceName text
+                '''CREATE TABLE EventsCustomChoices(EventID integer not null, ChoiceHeader text, ChoiceName text,
                 FOREIGN KEY(EventID) REFERENCES Events(EventID))''')
             self.cur.execute(
                 '''CREATE TABLE UserFeedback(EventID integer not null, UserID integer not null, Feedback text,
@@ -64,27 +67,36 @@ class Database:
     SQLite queries for users table
     '''
 
-    def insert_user(self, username, nusnet_id, house, telegram_id):
+    def insert_user(self, username, nusnet_id, house, telegram_id, telegram_handle, win_count):
         try:
             ## if user's full name and nusnet_id exists, do not insert
-            self.cur.execute("SELECT * FROM users WHERE username=? AND nusnet_id=? AND house=?", (username, nusnet_id, house,))
+            self.cur.execute("SELECT * FROM Users WHERE UserName=? AND NusnetId=? AND House=?", (username, nusnet_id, house,))
             if (len(self.cur.fetchall())):
                 return False
 
-            ## if telegram_id registered before, delete and insert
-            self.cur.execute("SELECT * FROM users WHERE telegram_id=?", (telegram_id,))
+            ## if telegram_id registered before, update all attributes of user
+            self.cur.execute("SELECT * FROM Users WHERE TelegramId=?", (telegram_id,))
             rows = self.cur.fetchall()
+
             if (len(rows)):
-                name = rows[0][USER_NAME]
-                self.cur.execute(
-                "DELETE FROM users WHERE telegram_id=?", (telegram_id,))
+                user_id = rows[0][USER_ID]
+                # old_win_count = rows[0][WIN_COUNT]
+                # self.cur.execute(
+                # "DELETE FROM Users WHERE TelegramId=?", (telegram_id,))
 
-                ## update details of user
-                self.cur.execute("UPDATE events_joined SET username=? WHERE telegram_id=?", (username, telegram_id))
-                # self.cur.execute("UPDATE user_feedback SET username=? WHERE telegram_id=?", (username, telegram_id))
+                # ## update details of user
+                # self.cur.execute("INSERT INTO Users(UserName, NusnetId, House, TelegramId, TelegramHandle, WinCount) VALUES(?,?,?,?,?,?)",
+                #                 (username, nusnet_id, house, telegram_id, telegram_handle, old_win_count))  #possible to insert old_user_id here?
+                # self.cur.execute("SELECT * FROM Users WHERE TelegramId=?", (telegram_id,))
+                # rows = self.cur.fetchall()
+                # new_user_id = rows[0][USER_ID]
+                # self.cur.execute("UPDATE EventsJoined SET UserID=? WHERE UserID=?", (new_user_id, old_user_id))
+                # # self.cur.execute("UPDATE user_feedback SET username=? WHERE telegram_id=?", (username, telegram_id))
+                self.cur.execute("UPDATE Users SET UserName=?, NusnetId=?, House=?, TelegramId=?, TelegramHandle=?, WinCount=? WHERE UserId=?", (username, nusnet_id, house, telegram_id, telegram_handle, win_count, user_id))
+            else:
+                self.cur.execute("INSERT INTO Users(UserName, NusnetId, House, TelegramId, TelegramHandle, WinCount) VALUES(?,?,?,?,?,?)",
+                                (username, nusnet_id, house, telegram_id, telegram_handle, win_count))                
 
-            self.cur.execute("INSERT INTO users(username, nusnet_id, house, telegram_id) VALUES(?,?,?,?)",
-                             (username, nusnet_id, house, telegram_id,))
             self.con.commit()
             return True
         except Exception as e:
@@ -94,7 +106,7 @@ class Database:
     def delete_user(self, telegram_id):
         try:
             self.cur.execute(
-                "DELETE FROM users WHERE telegram_id=?", (telegram_id,))
+                "DELETE FROM Users WHERE TelegramId=?", (telegram_id,))
             self.con.commit()    
         except Exception as e:
             print(e)
@@ -102,7 +114,7 @@ class Database:
 
     def query_all_users(self):
         try:
-            self.cur.execute("SELECT * FROM users")
+            self.cur.execute("SELECT * FROM Users")
             rows = self.cur.fetchall()
             arrayString = []
             for row in rows:
@@ -115,7 +127,7 @@ class Database:
 
     def query_user_name(self, telegram_id):
         try:
-            self.cur.execute("SELECT * FROM users WHERE telegram_id=?", (telegram_id,))
+            self.cur.execute("SELECT * FROM Users WHERE TelegramId=?", (telegram_id,))
             self.con.commit()
             rows = self.cur.fetchall()
             user_name = rows[0][USER_NAME]
@@ -128,7 +140,7 @@ class Database:
 
     def query_user_details(self, telegram_id):
         try:
-            self.cur.execute("SELECT * FROM users WHERE telegram_id=?", (telegram_id,))
+            self.cur.execute("SELECT * FROM Users WHERE TelegramId=?", (telegram_id,))
             self.con.commit()
             rows = self.cur.fetchall()
             user_details = rows[0]
@@ -361,8 +373,14 @@ class Database:
     '''
     def insert_events_custom_choices(self, event_name, choice_header, choice_name):
         try:
+            self.cur.execute("SELECT * FROM Events WHERE EventName=?", (event_name,))
+            rows = self.cur.fetchall()
+            if (rows):
+                event_id = rows[0][EVENT_ID]
+            else:
+                return
             self.cur.execute(
-                "INSERT INTO events_custom_choices(event_name, choice_header, choice_name) values (?,?,?)", (event_name, choice_header, choice_name,))
+                "INSERT INTO EventsCustomChoices(EventID, ChoiceHeader, ChoiceName) values (?,?,?)", (event_id, choice_header, choice_name,))
             self.con.commit()
             return True
         except Exception as e:
@@ -371,8 +389,14 @@ class Database:
 
     def delete_events_custom_choices(self, event_name):
         try:
+            self.cur.execute("SELECT * FROM Events WHERE EventName=?", (event_name,))
+            rows = self.cur.fetchall()
+            if (rows):
+                event_id = rows[0][EVENT_ID]
+            else:
+                return
             self.cur.execute(
-                "DELETE FROM events_custom_choices WHERE event_name=?", (event_name,))
+                "DELETE FROM EventsCustomChoices WHERE EventID=?", (event_id,))
             self.con.commit()
         except Exception as e:
             print(e)
@@ -381,7 +405,13 @@ class Database:
     def query_events_choices(self, event_name):
         try:
             LOCK.acquire(True)
-            self.cur.execute("SELECT * FROM events_custom_choices WHERE event_name = (?)", (event_name,))
+            self.cur.execute("SELECT * FROM Events WHERE EventName=?", (event_name,))
+            rows = self.cur.fetchall()
+            if (rows):
+                event_id = rows[0][EVENT_ID]
+            else:
+                return
+            self.cur.execute("SELECT * FROM EventsCustomChoices WHERE EventID = (?)", (event_id,))
             rows = self.cur.fetchall()
             arrayString = []
             for row in rows:
