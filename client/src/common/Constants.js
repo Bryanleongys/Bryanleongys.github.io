a21768a9e52d279932ccaaba679d306c78326523
bryanwhl <bryanwhl1999@gmail.com>
2022-03-13 03:43:19 +0800

    Add static build folder to root

---
+++ client/src/common/Constants.js
@@ -0,0 +1,13 @@
+// local server
+let baseURL = "https://18.224.63.50/api/";
+let username = "admin";
+let password = "rc4we1fare";
+
+export { baseURL, username, password };
+
+// // hosting server
+// let baseURL = "http://3.128.205.154/api/";
+// let username = "admin";
+// let password = "rc4welfare";
+
+// export { baseURL, username, password };
